7d253a6506d36873f9569e012986e7550310abea
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-07 16:05:37 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,8 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes)
+  movie = Movie.new
+  
 end
 
 def can_be_created_in_a_block(args = __)
